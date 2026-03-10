803c28d62565824be01ac6382c459357021c4e0b
Carlos Camarena <calcamaren@gmail.com>
2026-03-10 14:31:29 -0600

    Update dashboard.blade.php

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -1,10 +1,19 @@
 <x-app-layout>
     <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-        <script>
-            window.factucareDashboard = @json([
+        @php
+            $factucareDashboardPayload = [
                 'kpis' => $kpis ?? [],
-                'monthlyChart' => $monthlyChart ?? ['labels' => [], 'facturas' => [], 'complementos' => [], 'notas_credito' => []],
-            ]);
+                'monthlyChart' => $monthlyChart ?? [
+                    'labels' => [],
+                    'facturas' => [],
+                    'complementos' => [],
+                    'notas_credito' => [],
+                ],
+            ];
+        @endphp
+
+        <script>
+            window.factucareDashboard = @json($factucareDashboardPayload);
         </script>
 
         <!-- Dashboard actions -->
